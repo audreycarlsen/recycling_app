cc0695f3e4bef1696aaae828648baedeb7ce5c5a
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-31 13:32:27 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecyclingApp::Application.config.secret_key_base = '61098dfbf19f68e59385cc4adfed8701a775d8fed00cc1784b6a6f8ea057e95dec7360d38f59f336be17e7a0b4d194aebdc243a35a7b144a2b1615b84937e7aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RecyclingApp::Application.config.session_store :cookie_store, key: '_recycling_app_session'
