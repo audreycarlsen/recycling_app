43882934cc44028b5e0b78459907a8d480050152
Audrey Carlsen <audreycarlsen@gmail.com>
2014-04-04 11:22:44 -0700

    fixing git repo

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,12 @@
+class LocationsController < ApplicationController
+  def index
+    @locations = Location.where(:materials.in => ["Ribbon Cartridges"])
+
+    # TO DO: Look up these addresses by hand :(
+    @bad_locations = Location.all.where(latitude: nil)
+  end
+
+  def get_coordinates
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=1301+5th+Ave,+Seattle,+WA&sensor=true")
+  end
+end
