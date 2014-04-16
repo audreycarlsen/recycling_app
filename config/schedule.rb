1e56914e6a65b513b34c807b9df9b85deb4edced
Audrey Carlsen <audreycarlsen@gmail.com>
2014-04-16 12:00:48 -0700

    updated cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,6 +22,6 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every :wednesday, :at => '12pm' do
+every 5.minutes do
   rake "data_scraper"
 end
