8d6e0534c6475d8e57a301964526ddd88a6a00d9
Audrey Carlsen <audreycarlsen@gmail.com>
2014-04-22 16:44:04 -0700

    table toggle works again

--- app/assets/javascripts/table_toggle.js
+++ app/assets/javascripts/table_toggle.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.locations_table').on('click', '.locations_row', function(event) {
+  $('.location-table').on('click', '.locations_row', function(event) {
     $(this).siblings("#desc_" + this.id).toggle();
     $(this).find("span").toggle();
   });
