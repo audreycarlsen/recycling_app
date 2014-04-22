31394e7f5b2d25a539d5560c1f1fd30d6bb4065b
Audrey Carlsen <audreycarlsen@gmail.com>
2014-04-22 14:52:07 -0700

    map grays out for pick up and drop off

--- app/assets/javascripts/table_toggle.js
+++ app/assets/javascripts/table_toggle.js
@@ -3,4 +3,21 @@ $(document).ready(function() {
     $(this).siblings("#desc_" + this.id).toggle();
     $(this).find("span").toggle();
   });
+
+  $('.pick_up_tab').click(function() {
+    $('#map-canvas').addClass("opaque");
+    $('.hover-text').html('Pick-up locations not displayed.');
+    $('.hover-text').show();
+  });
+
+  $('.mail_in_tab').click(function() {
+    $('#map-canvas').addClass("opaque");
+    $('.hover-text').html('Mail-in locations not displayed.');
+    $('.hover-text').show();
+  });
+  
+  $('.drop_off_tab').click(function() {
+    $('#map-canvas').removeClass("opaque");
+    $('.hover-text').hide();
+  });
 });
